30e393a71712d8c851c3d4ce3b1e4dee678b1340
Reuben Thomas <reubenthomas123@gmail.com>
2023-12-07 12:44:13 +0800

    Hello

--- fetch_latest.sh
+++ fetch_latest.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Get all new changes
+# Config Directory
 dotfiles_directory=$(dirname "$(realpath "$BASH_SOURCE")")
 rm -rf "$dotfiles_directory"/.config
 cp -rf ~/.config "$dotfiles_directory"
 echo "The ~/.config directory has been copied to $dotfiles_directory"
-# Only allow tracked files
 cd "$dotfiles_directory"
 git clean --quiet -Xdf
+
+# Vim and Tmux
+cp ~/.vimrc "$dotfiles_directory"
+cp ~/.tmux.conf "$dotfiles_directory"
+
+
 
 
 
