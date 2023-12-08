9c1b242d29de41b2a29967fdc39341e8e8495617
Reuben Thomas <reubenthomas123@gmail.com>
2023-12-08 16:42:49 +0800

    Updates

--- fetch_latest.sh
+++ fetch_latest.sh
@@ -4,13 +4,14 @@
 dotfiles_directory=$(dirname "$(realpath "$BASH_SOURCE")")
 rm -rf "$dotfiles_directory"/.config
 cp -rf ~/.config "$dotfiles_directory"
-echo "The ~/.config directory has been copied to $dotfiles_directory"
 cd "$dotfiles_directory"
 git clean --quiet -Xdf
+echo "Updated contents of .config directory and copied to $dotfiles_directory"
 
 # Vim and Tmux
 cp ~/.vimrc "$dotfiles_directory"
 cp ~/.tmux.conf "$dotfiles_directory"
+echo "Updated latest .vimrc and .tmux.conf and copied to $dotfiles_directory"
 
 
 
