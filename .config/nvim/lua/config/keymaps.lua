4b970fb83e8a4d4bb82b51d3837fd5ee7c893668
Reuben Thomas <reubenthomas123@gmail.com>
2023-12-18 00:33:12 +0800

    color picker plugin for lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Color Picker Keymaps
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-c>", "<cmd>PickColor<cr>", opts)
+vim.keymap.set("i", "<C-c>", "<cmd>PickColorInsert<cr>", opts)
+vim.cmd([[hi FloatBorder guibg=NONE]]) -- if you don't want weird border background colors around the popup.
